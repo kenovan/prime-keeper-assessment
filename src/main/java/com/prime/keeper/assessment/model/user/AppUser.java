cd4ace91049596156c3b49a94143991dcb5d1569
Kenovan Cheok <kenovan.cheok@11street.my>
2018-04-29 01:16:30 +0800

    ignore password field to be return

--- src/main/java/com/prime/keeper/assessment/model/user/AppUser.java
+++ src/main/java/com/prime/keeper/assessment/model/user/AppUser.java
@@ -16,6 +16,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.prime.keeper.assessment.model.join.AppUserRole;
 
 @Entity
@@ -30,6 +31,7 @@ public class AppUser {
 	@Column(name = "user_name")
 	private String userName;
 	
+	@JsonIgnore
 	@Column(name = "user_password")
 	private String userPassword;
 	
